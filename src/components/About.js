0ac47d4054a5b353b5f1117356e8d9aa8907ccd4
Yejin Na <101006019+JustEddie@users.noreply.github.com>
2022-08-18 21:42:37 +1000

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,29 @@
 import React from "react";
+import user from "../data/user";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  // const bio = props.bio;
+  if(typeof props.bio === "string" && props.bio!==""){
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
+      {/* <p>{user.bio}</p> */}
+
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links />
+    </div>
+  );}
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links />
     </div>
   );
 }
+
+
 
 export default About;
